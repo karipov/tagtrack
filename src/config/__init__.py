d43cad6215bfa651938b94d02afd5b3e1e32159d
Komron <komron_aripov@cdl.ch>
2020-06-06 14:15:48 +0200

    dev configurations

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,3 +1,9 @@
 import json, pathlib  # noqa: E401
 
 CONFIG = json.load(open(pathlib.Path.cwd().joinpath("src/config/config.json")))
+
+
+if CONFIG.get('DEV_MODE'):
+    CONFIG = json.load(open(
+        pathlib.Path.cwd().joinpath("src/config/config_dev.json")
+    ))
